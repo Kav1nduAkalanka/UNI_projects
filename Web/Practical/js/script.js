d1485cb958e5f7f1544249508411fa9d39f2a5be
Kavi <akalankakavindu5@gmail.com>
2025-04-22 19:11:54 +0530

    uni projects

--- Web/Practical/js/script.js
+++ Web/Practical/js/script.js
@@ -19,3 +19,38 @@ document.body.addEventListener("click",function(){
 dropDownMenu.addEventListener("click",function(event){
     event.stopPropagation();
 });
+
+
+
+
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+        let i;
+        let slides = document.getElementsByClassName("slide");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slideIndex++;
+
+        if (slideIndex > slides.length) {
+            slideIndex = 1;
+        }
+
+        slides[slideIndex - 1].style.display = "block";
+
+        setTimeout(showSlides, 1500);
+}
+
+
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display ="flex";
+}
+
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display ="none";
+}
